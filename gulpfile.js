a66c9c9f1fa6f69e4012d8a6cd9e491066924318
Michael Burton <hello.michaelburton@gmail.com>
2015-04-18 13:46:59 +0100

    Add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
-var jsoncombine = require("gulp-jsoncombine");
- 
+var jsoncombine = require('gulp-jsoncombine');
+var del = require('del');
 
-
-gulp.task('default', function() {
-	gulp.src("./src/*.json")
-	    .pipe(jsoncombine("result.json",function(data){
+gulp.task('combine', ['clean'], function() {
+	gulp.src('./src/*.json')
+	    .pipe(jsoncombine('properties.json',function(data){
 	    	return new Buffer(JSON.stringify(data));
 	    }))
-	    .pipe(gulp.dest("./dist"));
+	    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function(cb) {
+	del(['dist'], cb);
 });
