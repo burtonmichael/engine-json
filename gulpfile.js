8f87aafb0c3a94cf3b055181af17e703ee2c1ccf
Michael Burton <michaelburton@rentalcars.com>
2015-04-22 17:04:06 +0100

    Removed need for additional categories.json

--- gulpfile.js
+++ gulpfile.js
@@ -15,55 +15,56 @@ var tempDir = './src/temp/';
 var destDir = './dist/';
 
 function getFolders(dir) {
-	return fs.readdirSync(dir)
-	.filter(function(file) {
-		return fs.statSync(path.join(dir, file)).isDirectory();
-	});
+    return fs.readdirSync(dir)
+        .filter(function(file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
 }
 
 gulp.task('merge', ['combine:categories'], function() {
-	gulp.src([srcDir + '/categories.json', tempDir + '/properties.json'])
-		.pipe(extend('properties.json'))
-	    .pipe(gulp.dest(destDir));
+    gulp.src([srcDir + '/categories.json', tempDir + '/properties.json'])
+        .pipe(extend('properties.json'))
+        .pipe(gulp.dest(destDir));
 
     return del([tempDir]);
 });
 
 gulp.task('combine:categories', ['combine:properties', 'clean:properies'], function() {
-	return gulp.src(tempDir + '/*.json')
-	    .pipe(jsoncombine('properties.json',function(data){
-	    	return new Buffer(JSON.stringify(data));
-	    }))
-	    .pipe(gulp.dest(tempDir));
+    return gulp.src(tempDir + '/*.json')
+        .pipe(jsoncombine('properties.json', function(data) {
+            return new Buffer(JSON.stringify(data));
+        }))
+        .pipe(gulp.dest(tempDir));
 });
 
 gulp.task('combine:properties', ['clean:temp'], function() {
-   var folders = getFolders(srcDir);
+    var folders = getFolders(srcDir);
 
-   var tasks = folders.map(function(folder) {
-		return gulp.src(path.join(srcDir, folder, '/**/*.json'))
-			.pipe(jsonlint())
-			.pipe(jsonlint.reporter())
-		    .pipe(jsoncombine(folder + '.json',function(data){
-		    	return new Buffer(JSON.stringify(data));
-		    }))
-			.pipe(transform(function(data) {
-		        return {
-		            "properties": data
-		        };
-		    }))
-			.pipe(gulp.dest(tempDir));
-   });
+    var tasks = folders.map(function(folder) {
+        return gulp.src(path.join(srcDir, folder, '/**/*.json'))
+            .pipe(jsonlint())
+            .pipe(jsonlint.reporter())
+            .pipe(jsoncombine(folder + '.json', function(data) {
+                return new Buffer(JSON.stringify(data));
+            }))
+            .pipe(transform(function(data) {
+                return {
+                    "parameter": folder,
+                    "properties": data
+                };
+            }))
+            .pipe(gulp.dest(tempDir));
+    });
 
-   return merge(tasks);
+    return merge(tasks);
 });
 
 gulp.task('clean:temp', function(cb) {
-	del([tempDir], cb);
+    del([tempDir], cb);
 });
 
 gulp.task('clean:properies', function(cb) {
-	del([destDir], cb);
+    del([destDir], cb);
 });
 
 gulp.task('default', ['merge']);
